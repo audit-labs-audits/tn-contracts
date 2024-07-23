0170b77814309bd131fb508dadde1c889f8f4e16
robriks <marsterlund@gmail.com>
2024-07-22 21:07:50 -0400

    Add git hooks via husky, install forge fmt via npm, upgrade to solidity 0.8.26

--- node/tinker.ts
+++ node/tinker.ts
@@ -31,9 +31,9 @@ async function main(): Promise<void> {
     // For example, if your chains array is ["Avalanche", "Fantom", "Moonbeam"], then http://localhost:8500/0 is the endpoint for the local Avalanche chain.
     const port = 8500;
 
-    async function deployUsdc(chain: Network): Promise<void> {
+    const deployUsdc = async (chain: Network): Promise<void> => {
       await chain.deployToken("Axelar Wrapped aUSDC", "aUSDC", 6, BigInt(1e22));
-    }
+    };
 
     await createAndExport({
       chainOutputPath: outputPath,
@@ -48,27 +48,30 @@ async function main(): Promise<void> {
 
   const bridge = async () => {
     // connect to TelcoinNetwork running on port 8545
-    const telcoinRpcUrl = "http://localhost:8545";
+    const telcoinRpcUrl = "http://localhost:8545/0";
     const telcoinProvider: JsonRpcProvider =
       new ethers.providers.JsonRpcProvider(telcoinRpcUrl);
 
     // set up Axelar infra and USDC token on TelcoinNetwork, instantiate Axelar <Network> wrapper around TN
     const pk = process.env.PK;
     if (!pk) throw new Error("Set private key string in .env");
-    const testerWallet: Wallet = new ethers.Wallet(pk); // does wallet need to be connected with provider?
+    const testerWallet: Wallet = new ethers.Wallet(pk, telcoinProvider); // does wallet need to be connected with provider?
     const networkSetup: NetworkSetup = {
       name: "Telcoin Network",
       chainId: 2017,
       ownerKey: testerWallet,
     };
     const tn: Network = await setupNetwork(telcoinProvider, networkSetup);
+    console.log(tn);
     const tnUSDC = await tn.getTokenContract("aUSDC");
+    console.log(tnUSDC);
 
     // mint tokens to testerWallet on ethereum
-    const ethRpcUrl = "http://localhost:8500";
+    const ethRpcUrl = "http://localhost:8500/0";
     const eth = await getNetwork(ethRpcUrl);
     await eth.giveToken(testerWallet.address, "aUSDC", BigInt(10e6));
     const ethUSDC = await eth.getTokenContract("aUSDC");
+    console.log(ethUSDC.balanceOf(testerWallet.address));
 
     // approve ethereum gateway to manage tokens
     const ethApproveTx = await ethUSDC
