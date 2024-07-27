34f2ed4e71dba96bd5933aa0e45af4f6655ea005
robriks <marsterlund@gmail.com>
2024-07-27 14:39:52 -0400

    :hammer: CREATE3 deployment script

--- script/TestnetDeployUniswapV2.s.sol
+++ script/TestnetDeployUniswapV2.s.sol
@@ -4,33 +4,42 @@ pragma solidity 0.8.26;
 import { Test, console2 } from "forge-std/Test.sol";
 import { Script } from "forge-std/Script.sol";
 import { LibString } from "solady/utils/LibString.sol";
+import { CREATE3 } from "solady/utils/CREATE3.sol";
 import { IUniswapV2Factory } from "script/uniswap/interfaces/IUniswapV2Factory.sol";
 import { IUniswapV2Router02 } from "script/uniswap/interfaces/IUniswapV2Router02.sol";
+import { UniswapV2FactoryBytecode } from "script/uniswap/precompiles/UniswapV2FactoryBytecode.sol";
+import { UniswapV2Router02Bytecode } from "script/uniswap/precompiles/UniswapV2Router02Bytecode.sol";
 import { WTEL } from "../src/WTEL.sol";
 
-contract TestnetDeployUniswapV2 is Script {
+contract TestnetDeployUniswapV2 is Script, UniswapV2FactoryBytecode, UniswapV2Router02Bytecode {
+
     address wTEL;
     address feeToSetter_;
     address admin;
+    bytes32 factorySalt;
+    bytes32 routerSalt;
 
     IUniswapV2Factory uniswapV2Factory;
     IUniswapV2Router02 uniswapV2Router02;
 
     function setUp() public {
-        // wTEL = ;
-        // feeToSetter = address(0x0);
+        // todo: read JSON for these
+        wTEL = 0x5c78ebbcfdc8Fd432C6D7581F6F8E6B82079f24a;
         admin = 0xc1612C97537c2CC62a11FC4516367AB6F62d4B23;
         feeToSetter_ = admin;
+
+        factorySalt = bytes32(bytes("UniswapV2Factory"));
+        routerSalt = bytes32(bytes("UniswapV2Router02"));
     }
 
     function run() public {
         // deploy v2 core contracts
-        feeToSetter_ = admin;
-        // uniswapV2Factory = IUniswapV2Factory(vm.deployCode("script/uniswap/precompiles/UniswapV2Factory.json")); //
-        // feeToSetter
+        //todo constructor: feeToSetter
+        uniswapV2Factory = IUniswapV2Factory(CREATE3.deployDeterministic(UNISWAPV2FACTORY_BYTECODE, factorySalt));
+
         // deploy v2 periphery contracts
-        // uniswapV2Router02 = IUniswapV2Router02(vm.deployCode("script/uniswap/precompiles/UniswapV2Router02.json"));
-        // //address(uniswapV2Factory), wTEL
+        //todo constructor: (address(uniswapV2Factory), wTEL)
+        uniswapV2Router02 = IUniswapV2Router02(CREATE3.deployDeterministic(UNISWAPV2ROUTER02_BYTECODE, routerSalt));
 
         // asserts
         assert(address(uniswapV2Factory).code.length != 0);
@@ -49,17 +58,5 @@ contract TestnetDeployUniswapV2 is Script {
                 "UniswapV2Router02: ", LibString.toHexString(uint256(uint160(address(uniswapV2Router02))), 20)
             )
         );
-    }
-
-    // taken from https://ethereum.stackexchange.com/q/132029/86303
-    function deployCode(bytes memory bytecode) internal returns (address payable addr) {
-        assembly {
-            addr := create(0, add(bytecode, 0x20), mload(bytecode))
-
-            if iszero(extcodesize(addr)) {
-                returndatacopy(0, 0, returndatasize())
-                revert(0, returndatasize())
-            }
-        }
     }
 }
