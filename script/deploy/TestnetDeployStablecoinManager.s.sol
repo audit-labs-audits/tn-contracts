220703dccd2b31138977bce4cd385aafa19ddf4a
robriks <marsterlund@gmail.com>
2024-09-23 00:23:07 -0400

    Improve faucet deploy script for network restarts, asserts & debugging, + enable all xyzs flag

--- script/deploy/TestnetDeployStablecoinManager.s.sol
+++ script/deploy/TestnetDeployStablecoinManager.s.sol
@@ -9,7 +9,7 @@ import { StablecoinHandler } from "telcoin-contracts/contracts/stablecoin/Stable
 import { Stablecoin } from "telcoin-contracts/contracts/stablecoin/Stablecoin.sol";
 import { StablecoinManager } from "../../src/StablecoinManager.sol";
 import { WTEL } from "../../src/WTEL.sol";
-import { Deployments } from "../../deployments/Deployments.sol";
+import { Deployments, DETERMINISTIC_FIRST_FAUCET_IMPL_DATA, DETERMINISTIC_FAUCET_PROXY_DATA } from "../../deployments/Deployments.sol";
 
 /// @dev Usage: `forge script script/deploy/TestnetDeployTokens.s.sol --rpc-url $TN_RPC_URL -vvvv --private-key
 /// $ADMIN_PK`
@@ -18,8 +18,9 @@ contract TestnetDeployStablecoinManager is Script {
     StablecoinManager stablecoinManager;
 
     bytes32 stablecoinManagerSalt; // used for both impl and proxy
+    // true: enable $TEL and all `stables` | false: enable $TEL only
+    bool enableAllXYZs = false;
     address[] stables;
-    StablecoinManager.eXYZ[] eXYZs;
     uint256 maxLimit;
     uint256 minLimit;
 
@@ -30,6 +31,7 @@ contract TestnetDeployStablecoinManager is Script {
     address[] faucets; // will contain above
     uint256 dripAmount;
     uint256 nativeDripAmount;
+    uint256 lowBalanceThreshold;
 
     Deployments deployments;
     address admin; // admin, support, minter, burner role
@@ -48,12 +50,13 @@ contract TestnetDeployStablecoinManager is Script {
         minLimit = 1000;
         dripAmount = 100e6; // 100 units of the stablecoin (decimals == 6)
         nativeDripAmount = 1e18; // 1 $TEL
+        lowBalanceThreshold = 10_000;
 
         // populate stables array
         stables.push(deployments.eAUD);
         stables.push(deployments.eCAD);
         stables.push(deployments.eCHF);
-        // stables.push(deployments.eEUR);
+        stables.push(deployments.eEUR);
         stables.push(deployments.eGBP);
         stables.push(deployments.eHKD);
         stables.push(deployments.eMXN);
@@ -62,11 +65,6 @@ contract TestnetDeployStablecoinManager is Script {
         stables.push(deployments.eSDR);
         stables.push(deployments.eSGD);
 
-        // DEPRECATED: populate eXYZs
-        // for (uint256 i; i < stables.length; ++i) {
-        //     eXYZs.push(StablecoinHandler.eXYZ(true, maxLimit, minLimit));
-        // }
-
         faucets.push(faucet0);
         faucets.push(faucet1);
         faucets.push(faucet2);
@@ -74,33 +72,53 @@ contract TestnetDeployStablecoinManager is Script {
     }
 
     function run() public {
+        // uncomment for debugging
+        // stablecoinManager = StablecoinManager(payable(deployments.StablecoinManager)); 
+        // stablecoinManagerImpl = StablecoinManager(payable(deployments.StablecoinManagerImpl));
+        
         vm.startBroadcast();
 
-        stablecoinManagerImpl = new StablecoinManager{ salt: stablecoinManagerSalt }();
-        bytes memory initData = abi.encodeWithSelector(
-            StablecoinManager.initialize.selector,
-            StablecoinManager.StablecoinManagerInitParams(
-                admin, admin, stables, maxLimit, minLimit, faucets, dripAmount, nativeDripAmount
-            )
-        );
-        stablecoinManager = StablecoinManager(
-            payable(address(new ERC1967Proxy{ salt: stablecoinManagerSalt }(address(stablecoinManagerImpl), initData)))
-        );
+        // deploy the deterministic faucet proxy's first implementation to prevent proxy revert on deployment
+        (bool implRes,) = deployments.ArachnidDeterministicDeployFactory.call(DETERMINISTIC_FIRST_FAUCET_IMPL_DATA);
+        require(implRes); // first implementation address: `0x857721c881fc26e4664a9685d8650c0505997672`
 
-        // grant minter role to StablecoinManager on all tokens
+        // deploy the deterministic faucet proxy using first constructor args (impl, initializeData)
+        (bool proxyRes, bytes memory proxyRet) = deployments.ArachnidDeterministicDeployFactory.call(DETERMINISTIC_FAUCET_PROXY_DATA);
+        require(proxyRes);
+        address payable stablecoinManagerAddress = payable(address(bytes20(proxyRet)));
+        stablecoinManager = StablecoinManager(stablecoinManagerAddress);
+
+        // deploy latest faucet version
+        stablecoinManagerImpl = new StablecoinManager{ salt: stablecoinManagerSalt }();
+
+        // perform upgrade to current faucet version and set native drip amount since TEL is enabled by default
+        bytes memory setNativeDripAmountCall = abi.encodeWithSelector(StablecoinManager.setNativeDripAmount.selector, nativeDripAmount);
+        stablecoinManager.upgradeToAndCall(address(stablecoinManagerImpl), setNativeDripAmountCall);
+
+        stablecoinManager.setDripAmount(dripAmount);
+        stablecoinManager.setLowBalanceThreshold(lowBalanceThreshold);
+        stablecoinManager.UpdateXYZ(stablecoinManager.NATIVE_TOKEN_POINTER(), true, maxLimit, minLimit);
+
+        // grant minter role to StablecoinManager on all tokens and disables XYZs if `!enableAllXYZs`
         bytes32 minterRole = keccak256("MINTER_ROLE");
         for (uint256 i; i < stables.length; ++i) {
             Stablecoin(stables[i]).grantRole(minterRole, address(stablecoinManager));
+
+            if (!enableAllXYZs && stablecoinManager.isEnabledXYZ(stables[i])) {
+                stablecoinManager.UpdateXYZ(stables[i], false, maxLimit, minLimit);
+            }
         }
 
         vm.stopBroadcast();
 
         // asserts
-        assert(stablecoinManager.getEnabledXYZs().length == 10);
-        assert(stablecoinManager.getEnabledXYZsWithMetadata().length == 10);
+        assert(stablecoinManager.isEnabledXYZ(address(0x0)));
+        assert(stablecoinManager.getEnabledXYZs().length == 0);
+        assert(stablecoinManager.getEnabledXYZsWithMetadata().length == 0);
         assert(minterRole == Stablecoin(stables[0]).MINTER_ROLE());
         for (uint256 i; i < stables.length; ++i) {
             assert(Stablecoin(stables[i]).hasRole(minterRole, address(stablecoinManager)));
+            assert(stablecoinManager.isEnabledXYZ(stables[i]) == enableAllXYZs);
         }
         for (uint256 i; i < faucets.length; ++i) {
             assert(stablecoinManager.hasRole(stablecoinManager.FAUCET_ROLE(), faucets[i]));
