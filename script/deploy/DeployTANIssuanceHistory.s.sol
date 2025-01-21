2e32b4f94134a29c7aafcaa6c67a169f61acfe30
robriks <marsterlund@gmail.com>
2025-01-21 14:23:27 -0500

    TANIssuanceHistory deploy script ready, awaiting deployed SimplePlugin from external private repo

--- script/deploy/DeployTANIssuanceHistory.s.sol
+++ script/deploy/DeployTANIssuanceHistory.s.sol
@@ -7,19 +7,14 @@ import { LibString } from "solady/utils/LibString.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { Deployments } from "../../deployments/Deployments.sol";
 import { TANIssuanceHistory } from "../../src/issuance/TANIssuanceHistory.sol";
-// todo: get access to repos for these contracts
-// import { SimplePlugin } from "";
-// import { StakingModule } from "";
 
-contract TestnetDeployTANIssuanceHistory is Script {
+contract DeployTANIssuanceHistory is Script {
     TANIssuanceHistory tanIssuanceHistory;
-    // SimplePlugin tanIssuancePlugin;
-
+    
     // config
     Deployments deployments;
-    // polygon addresses for `SimplePlugin::constructor()`
-    IERC20 tel = IERC20(0xdF7837DE1F2Fa4631D716CF2502f8b230F1dcc32);
-    // StakingModule stakingModule = StakingModule(0x92e43Aec69207755CB1E6A8Dc589aAE630476330);
+    IERC20 tel;
+    SimplePlugin tanIssuancePlugin;
 
     function setUp() public {
         string memory root = vm.projectRoot();
@@ -27,14 +22,38 @@ contract TestnetDeployTANIssuanceHistory is Script {
         string memory json = vm.readFile(path);
         bytes memory data = vm.parseJson(json);
         deployments = abi.decode(data, (Deployments));
+
+        // both mocks and prod contracts use canonical TEL
+        tel = IERC20(0xdF7837DE1F2Fa4631D716CF2502f8b230F1dcc32);
+
+        // polygon mock plugin
+        tanIssuancePlugin = SimplePlugin(/**address tbd */);
     }
 
     function run() public {
         vm.startBroadcast();
 
-        // tanIssuancePlugin = new SimplePlugin{salt: bytes32(0x0)}()
-        // tanIssuanceHistory = new TANIssuanceHistory{salt: bytes32(0x0)}(tanIssuancePlugin);
+        tanIssuanceHistory = new TANIssuanceHistory{salt: bytes32(0x0)}(tanIssuancePlugin);
 
         vm.stopBroadcast();
+
+        // asserts
+        assert(tanIssuanceHistory.owner() == msg.sender);
+        assert(tanIssuanceHistory.tanIssuancePlugin() == tanIssuancePlugin);
+        assert(tanIssuanceHistory.clock() == block.number);
+
+        // logs
+        string memory root = vm.projectRoot();
+        string memory dest = string.concat(root, "/deployments/deployments.json");
+        vm.writeJson(
+            LibString.toHexString(uint256(uint160(address(tanIssuancePlugin))), 20),
+            dest,
+            ".TanIssuancePlugin"
+        );
+        vm.writeJson(
+            LibString.toHexString(uint256(uint160(address(tanIssuanceHistory))), 20),
+            dest,
+            ".TanIssuanceHistory"
+        );
     }
 }
